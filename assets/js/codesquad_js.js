1efdae854baf0e2858003643d13c0c99c4b80d22
Andrew O <andrew@codesquad.org>
2018-09-13 16:53:48 -0400

    Added Bootstrap & custom JS files

---
+++ assets/js/codesquad_js.js
@@ -0,0 +1,8 @@
+// GOAL IS TO CHANGE TEXT IN RED BAR BASED ON SCREEN SIZE
+// For big screens, write "Energize your talent pipeline..."
+// For mobile phones, write "and stable careers in low-income communities"
+// while hiding the buttons and sub-head over the main banner image
+// var redTxtHome = document.getElementById("redTxtHome").innerHTML;
+
+// THE LINE BELOW CHANGES THE TEXT; STILL NEED TO ADD SCREEN-SIZE TEST
+// document.getElementById("redTxtHome").innerHTML = "New text!";
